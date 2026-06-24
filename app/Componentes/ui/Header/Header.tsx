439751d98ccad68d8a5447a43c441d31048735c3
asus_gaming <workformy110@gmail.com>
2026-06-25 04:09:05 +0530

    init

---
+++ app/Componentes/ui/Header/Header.tsx
@@ -0,0 +1,19 @@
+
+
+export default function Header() {
+    return(
+        <>
+            <header/>
+                <section className="
+                bg-bannerImg
+                bg-repeat
+                bg-cover
+                bg-bottom
+                w-auto
+                h-auto
+                ">
+
+                </section>
+        </>
+    )
+}
---
+++ app/Componentes/ui/Pages/Home/Home.tsx
@@ -0,0 +1,38 @@
+import Video from 'next-video';
+import myVideo from '@videos/pizza1.mp4';
+import useSWR from 'swr'
+
+
+export default function Home() {
+    return(
+        <>
+            <main className="relative w-screen h-screen overflow-hidden">
+                <Video
+                    src={myVideo}
+                    autoplay
+                    muted
+                    loop
+                    playsInline
+                    controls={false}
+                    className="w-full h-full object-cover"
+                >
+                </Video>
+                <h1 className="
+                absolute
+                left-0
+                bottom-0
+                w-full
+                px-12
+                pd-12
+                pt-24
+                text-white
+                text-[34px]
+                font-bold
+                ">
+                    Next.js background video
+                </h1>
+            </main>
+
+        </>
+    )
+}

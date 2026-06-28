4dc4b73fe865cd7943b266dca766c4197643a36b
asus_gaming <workformy110@gmail.com>
2026-06-28 05:37:39 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/app/Componentes/ui/Header/Header"
+import Home from "@/app/Componentes/ui/Pages/Home/Home";
+import Navbar from "@/app/Componentes/ui/Header/Navbar/Navbar";
+
+export default function MainPage() {
+  return (
+      <>
+       <Header/>
+          <Navbar/>
+          <Home/>
+      </>
+  );
+}
